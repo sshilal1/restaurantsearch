578d9a825b7a3d609d184eba0a0774d3e16c8c55
Stephen Shilale <sshilal1@binghamton.edu>
2017-11-08 22:30:14 -0500

    string format date and address

--- client-app/src/components/RestaurantEntry.js
+++ client-app/src/components/RestaurantEntry.js
@@ -6,12 +6,16 @@ import seal from '../images/seals/Nyc-seal-blue.png';
 class RestaurantEntry extends Component {
 	render() {
 
-		const { name,cuisine,street,phone,grade,building,zipcode,imageUrl } = this.props;
+		const { name,cuisine,street,phone,grade,gradeDate,building,zipcode,imageUrl,boro } = this.props;
 
 		var standard = grade === 'Not Yet Graded' ? 'GP' : grade;
-
 		var rank = require('../images/letters/'+standard.toLowerCase()+'.png');
 		var dollars = '$'.repeat(3);
+		var number = `(${phone.substring(0,3)}) - ${phone.substring(3,6)} - ${phone.substring(6,10)}`;
+		var address = `${building} ${street}, ${boro}, NY ${zipcode}`;
+		var day = gradeDate.substring(8,10);
+		var month = gradeDate.substring(5,7);
+		var year = gradeDate.substring(0,4);
 
 		var textStyle = {
 			fontSize: ".9em",
@@ -26,12 +30,12 @@ class RestaurantEntry extends Component {
 				<CardBody>
 					<h6 style={{fontWeight:"bold",color:"#14608a"}}>{name}</h6>
 					<CardText style={{...textStyle, color:"grey"}}>{cuisine} &middot; {dollars}</CardText>
-					<CardText style={textStyle}>{street}</CardText>
-					<CardText style={textStyle}>{phone}</CardText>
+					<CardText style={textStyle}>{address}</CardText>
+					<CardText style={textStyle}>{number}</CardText>
 					<div className="rank-box">
 						<img className="rank-img" src={seal} alt="search"/>
 						<img className="rank-img" src={rank} alt="search"/>
-						<div className="inspection-date">07 &middot; 12 &middot; 2017</div>
+						<div className="inspection-date">{month} &#183; {day} &#183; {year}</div>
 					</div>
 				</CardBody>
 				
